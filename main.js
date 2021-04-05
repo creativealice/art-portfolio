a0ef5aa73175d651e8d3f5664040c9a1415d1a88
creativealice <81888433+creativealice@users.noreply.github.com>
2021-04-05 20:10:13 +1000

    Final changes, code clean-up
    -cleaned up code
    -changed animation on title to on hover with anchor link
    -smooth scroll added

--- main.js
+++ main.js
@@ -10,127 +10,70 @@ emailIcon.addEventListener('mouseout', function() {
     })
 
 //Parallax
-var bg = document.querySelector('#background');
-var spirit = document.querySelector('#spirit');
-var fg = document.querySelector('#foreground');
-var text = document.querySelector('#text');
+var title = document.querySelector('#title');
 
 window.addEventListener('scroll', function() {
+    var bg = document.querySelector('#background');
+    var spirit = document.querySelector('#spirit');
+    var fg = document.querySelector('#foreground');
     var value = window.scrollY;
 
     bg.style.top = value * 0.15 + 'px';
     spirit.style.left = -value * 0.15 + 'px';
     fg.style.top = value * 0.15 + 'px';
-    text.style.top = value * 1 + 'px';
+    title.style.top = value * 1 + 'px';
 })
 
-//Trigger thank you message on send
-var button = document.querySelector('button')
-var connectForm = document.querySelector('form')
-var letsConnectTitle = document.querySelector('#connect')
+//Title on hover effect
+title.addEventListener('mouseover', function() {
+    title.classList.add('text-shadow-pop-right');
+    })
 
+title.addEventListener('mouseout', function() {
+    title.classList.remove('text-shadow-pop-right');
+    })
+
+//Trigger thank you message on send
+
+var button = document.querySelector('button')
 button.addEventListener('click', function() {
+    var connectForm = document.querySelector('form')
+    var letsConnectTitle = document.querySelector('#connect')
+
     letsConnectTitle.style.display = 'none'
     connectForm.innerHTML = `<p id="thank-you-message">Thanks for getting in touch! I'll get back to you as soon as I can.</p><br>`    
 }) 
 
-//Function to show lightbox on thumbnail click
+//Function to close lightbox
 
-var lightbox = document.querySelector('.lightbox')
-// var clickWhalesOverlay = document.querySelector('#dusk-whales')
-// var clickForestSpiritOverlay = document.querySelector('#overlay-forest-spirit')
-var lightBoxImage = document.querySelector('.lightbox-image')
-var closeBtn = document.querySelector('.close')
-var nextBtn = document.querySelector('.next')
-var prevBtn = document.querySelector('.prev')
-
-
-// clickWhalesOverlay.addEventListener('click', function () {
-//     lightbox.style.display = "block"
-//     lightBoxImage.src = (`./images/${clickWhalesOverlay.id}.jpg`)
-// })
-
-// clickForestSpiritOverlay.addEventListener('click', function () {
-//     lightbox.style.display = "block"
-//     lightBoxImage.src = ('./images/Forest-Spirit_Alice-Cao.jpg')
-// })
-
-
-//function to close lightbox
 //find the close button, on click, change display of lightbox.style to 'none'
+var closeBtn = document.querySelector('.close')
+
 closeBtn.addEventListener('click', function() {
     lightbox.style.display = "none"
 })
 
-nextBtn.addEventListener('click', function() {
+//Function to show relevant lightbox slide image on thumbnail click
 
-})
-
-//write eventlistener for prev slide if it works
-
-// function setThumbnailOnClick(thumbnail) {
-//     thumbnail.addEventListener('click', function() {
-//         lightbox.style.display = "block"
-//         lightBoxImage.src = (`./images/${thumbnail.id}.jpg`)
-//     })
-// }
-
-// var thumbnailList = document.querySelectorAll('.overlay')
-
-// for (var i = 0; i < thumbnailList.length; i++) {
-//     setThumbnailOnClick(thumbnailList[i])
-// }
+var lightbox = document.querySelector('.lightbox')
+var lightBoxImage = document.querySelector('.lightbox-image')
 
 
+function setThumbnailOnClick(thumbnail) {
+    thumbnail.addEventListener('click', function() {
 
-// allOverlay.forEach(overlay => {
-//     overlay.addEventListener('click', function() {
-//         lightbox.style.display = "block"
-//         lightBoxImage.src = (`./images/${overlay.id}.jpg`)
-//     })
-// })
+//Makes lightbox visible
+        lightbox.style.display = "block"
 
-
-// 1) We grab all images with class 'overlay' and add the same onclick event listener to them in a for loop
-// 2) Each onclick event listener uses the id as the filename suffix like lightBoxImage.src = ('./images/' + overlayImage.id + '.jpg')
-// 3) Each event listener sets a variable currentLightBoxImageIndex which is the index of the overlay array list of images
-// 4) Add event listener to next and prev buttons to add 1 or minus 1 to that index (obviously if it goes below 0 it loops back to the last image and vice versa)
-
-var thumbnailList = document.querySelectorAll('.overlay') //get array of images
-var currentLightboxImageNumber; 
-
-function setThumbnailOnClick(thumbnailNumber) {
-    thumbnailList[thumbnailNumber].addEventListener('click', function() {
-        currentLightboxImageNumber = thumbnailNumber
-        showLightboxImage(currentLightboxImageNumber) 
-        lightbox.style.display = "block" //show the image by changing default display of none to block
-        lightBoxImage.src = (`./images/${thumbnailList[currentLightboxImageNumber].id}.jpg`)
+//Changes image to dynamic source URL based on the clicked thumbnail's number and thus ID. All images were named the same as the ID allowing this.
+        lightBoxImage.src = (`./images/${thumbnail.id}.jpg`)
     })
 }
 
-//function that grabs the currentImage
-function showLightboxImage() {
-    lightbox.style.display = "block" //show the image by changing default display of none to block
-    lightBoxImage.src = (`./images/${thumbnailList[currentLightboxImageNumber].id}.jpg`)
-}
+//get array of images
+var thumbnailList = document.querySelectorAll('.overlay')
 
+//for loop which goes through each array item and performs the function to get the assigned number of the item to use in the function setThumbnailOnClick
 for (var i = 0; i < thumbnailList.length; i++) {
-    setThumbnailOnClick(i)
+    setThumbnailOnClick(thumbnailList[i])
 }
-
-
-
-
-
-
-
-
-// nextBtn.addEventListener('click', function() {
-//     currentLightboxImage = (currentLightboxImage + 1) % thumbnailList.length
-//     showLightboxImage(currentLightboxImage)
-// })
-
-// prevBtn.addEventListener('click', function() {
-//     currentLightboxImage = (currentLightboxImage - 1) % thumbnailList.length
-//     showLightboxImage(currentLightboxImage)
-// })
